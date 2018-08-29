b1f834a72ba9b186caf4e48682bbe35a8d4604b8
Ashley Cook <ashley.m.d.cook@gmail.com>
2018-08-29 21:35:50 +0100

    Completed requested exercises.

--- jsexercises/javascript-exercises/sumAll/sumAll.js
+++ jsexercises/javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+	var sum_total = 0;
+  
+  if(number1 < 0 || number2 < 0) {
+  	return "ERROR";
+  } else if (typeof number1 != "number" || typeof number2 != "number"){
+  	return "ERROR";
+  }
+  var smallest_number = '';
+  var largest_number = '';
+  
+  if(number1 > number2){
+  	largest_number = number1;
+    smallest_number = number2;
+  } else if (number2 > number1) {
+    largest_number = number2;
+    smallest_number = number1;
+  } else {
+  	largest_number = number1;
+    smallest_number = number2;
+  }
+  
+  while(smallest_number <= largest_number){
+    sum_total += smallest_number;
+    smallest_number += 1;
+  }
+  
+  return sum_total;
+  
 }
 
 module.exports = sumAll
--- jsexercises/javascript-exercises/tempConversion/tempConversion.js
+++ jsexercises/javascript-exercises/tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempreture) {
+  var farenheit = 0;
+  
+  farenheit = (tempreture - 32) * 5/9;
+  return Number(farenheit.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(tempreture) {
+  var farenheit = 0;
+  
+  farenheit = tempreture * 9/5 + 32;
+  return Number(farenheit.toFixed(1));
 }
 
 module.exports = {
