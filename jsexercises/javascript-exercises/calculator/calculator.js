db1971e5babe3a61421a7a3409def2708a91a367
Ash <ashley.m.d.cook@gmail.com>
2018-10-04 00:06:56 +0100

    Calculator tasks

--- jsexercises/javascript-exercises/calculator/calculator.js
+++ jsexercises/javascript-exercises/calculator/calculator.js
@@ -17,12 +17,21 @@ var sum = array.reduce(function (accumulator, currentValue) {
 return sum;
 }
 
-function multiply () {
-	
+function multiply(array){
+var sum = array.reduce(function (accumulator, currentValue) {
+  return accumulator * currentValue;
+});
+return sum;
 }
 
-function power() {
-	
+function power(base, exponent){
+	var count = (exponent - 2);
+	var initialValue = (base * base);
+	while (count > 0){
+  	initialValue *= base 
+    count -= 1;
+	}
+  return initialValue;
 }
 
 function factorial() {
