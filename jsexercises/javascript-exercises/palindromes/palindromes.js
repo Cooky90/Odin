52cce2b4d48be13836d0c0a013918c1e723a049f
Ash <ashley.m.d.cook@gmail.com>
2018-10-09 01:21:40 +0100

    Finished palindrome exercsise.

--- jsexercises/javascript-exercises/palindromes/palindromes.js
+++ jsexercises/javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(words) {
+ var replacedWords = words.replace(/[\W_]+/g, "");
+ var splitWords = replacedWords.split("");
+   if (splitWords.reverse().join("").toLowerCase() == replacedWords.split().reverse().join("").toLowerCase()){
+   return true;
+   } else {
+   return false;
+   }
 }
 
 module.exports = palindromes
