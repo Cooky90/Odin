e7927b22367603c655e06aff233ab8298e500518
Ash <ashley.m.d.cook@gmail.com>
2018-10-21 23:00:26 +0100

    First Ruby exercises

--- Ruby/learn_ruby-master/learn_ruby-master/03_simon_says/simon_says.rb
+++ Ruby/learn_ruby-master/learn_ruby-master/03_simon_says/simon_says.rb
@@ -1,1 +1,16 @@
-#write your code here
+def echo(string)
+	return string
+end
+
+def shout(string)
+	return string.upcase
+end
+
+def repeat(string,number = 1)
+	final_string = string
+	until number == 0
+		final_string += " " + string
+		number -= 1
+	end
+	return final_string
+end
